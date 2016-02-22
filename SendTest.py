a36b17ad6237661064bdbeb5c0ee31a05929923c
dwicke <drewwicke@gmail.com>
2016-02-22 11:23:39 -0500

    blah

--- SendTest.py
+++ SendTest.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 from ConnectionManager import ConnectionManager
 
-# tcpCon = ConnectionManager('tcp')
-# tcpCon.addClient('localhost', 9001)
-# tcpCon.send('HIIH i am tcp yo')
-# tcpCon.recv()
+
 
 
 udpCon = ConnectionManager('udp', )
 udpCon.addClient('localhost', 9000)
 udpCon.send('HIIH I am udp')
 udpCon.recv()
+# tcpCon = ConnectionManager('tcp')
+# tcpCon.addClient('localhost', 9001)
+# tcpCon.send('HIIH i am tcp yo')
+# tcpCon.recv()
