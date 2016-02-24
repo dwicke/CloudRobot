5ad94de17216bdf8eedd300c199dadc076dc5364
dwicke <drewwicke@gmail.com>
2016-02-24 11:38:32 -0500

    added a print

--- BountyHunterLearner.py
+++ BountyHunterLearner.py
@@ -44,7 +44,7 @@ class BountyHunterLearner(object):
             bestTask = None
             maxI = -1
             for k, v in tasks.iteritems():
-                print 'The currentBounty = %f' % (v['currentBOunty'])
+                print 'The currentBounty = %f' % (v['currentBounty'])
                 I = (v['currentBounty']/self.TTable.getQValue(k, 0))*self.PTable.getQValue(k, 0)
                 if I > maxI:
                     maxI = I
