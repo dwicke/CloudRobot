5cb812de2ee0dcaf7cf5fce282502132b4443936
dwicke <drewwicke@gmail.com>
2016-02-29 14:56:37 -0500

    increased the sleeping and prob

--- VisualServoing.py
+++ VisualServoing.py
@@ -41,7 +41,7 @@ class VisualServoing(object):
         self.MSGLEN = self.WIDTH*self.HEIGHT*self.CHANNELS
         self.BONDSMANPORT = 14000
         self.taskName = taskName
-        self.probThresh = .07
+        self.probThresh = .35
 
 
     def doTask(self):
